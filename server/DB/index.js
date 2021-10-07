b79bdd55100325db49f8b40e3908482f9ac5f444
SHUANESH <shuanesh1817@gmail.com>
2021-10-07 22:17:34 +0300

    firs commit

---
+++ server/DB/index.js
@@ -0,0 +1,24 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const dbConnection = process.env.dbConnection;
+// const dbConnection_test = process.env.dbConnection_test;
+const mongoose = require("mongoose");
+const chalk = require("chalk");
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+};
+
+mongoose
+  .connect(dbConnection, options)
+  .then(() => {
+    console.log(chalk.green("mongoDB connected"));
+  })
+  .catch((err) => {
+    console.log(chalk.red("Connection error", err.message));
+  });
+
+const db = mongoose.connection;
+module.exports = db;
---
+++ server/server.js
@@ -0,0 +1,31 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const PORT = process.env.PORT || 8080;
+const chalk = require("chalk");
+const path = require("path");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//import route
+
+//DB connection
+const db = require("./DB");
+db.on("error", () => {
+  console.log(chalk.red("Connection error"));
+});
+
+// use route
+
+
+app.listen(PORT, () => {
+  console.log(
+    `${chalk.green("Share_me_app")} ${chalk.yellow(
+      "live and up on port"
+    )} ${chalk.blue(PORT)}`
+  );
+});
+
