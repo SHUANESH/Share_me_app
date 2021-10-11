68d617a2c8ce51359e361956143626d61898a1e5
natidese <natidese0@gmail.com>
2021-10-11 21:22:54 +0300

    AppRouter

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
+import AppRouter from "./AppRouter/AppRouter";
+import { BrowserRouter as Router } from "react-router-dom";
+
 
 const App = () => {
   return <div className="App">
+    <Router>
+      <AppRouter/>
+    </Router>
 
   </div>;
 };
-
-export default App;
