1455f852afc338c23bc5be9fc6e9f09fbbac0c38
natidese <natidese0@gmail.com>
2021-10-13 14:18:50 +0300

    updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,7 @@
 import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import Routing from "./routing/Routing";
-import Contact from "./components/pages/contact/Contact"
-import Navbar from "./components/features/navbar/Navbar";
-import Footer from "./components/features/footer/Footer"
+
 
 
 const App = () => {
@@ -12,8 +10,7 @@ const App = () => {
     <div className="App">
      
     <Router>
-     <Navbar />
-      {/* <Routing/>  */}
+      <Routing/> 
     </Router>
   </div>
 
--- client/src/components/pages/home/Home.jsx
+++ client/src/components/pages/home/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import './home.scss';
 import News from '../../features/news/news'
-import Navbar from '../../features/navbar/Navbar'
 import About from '../../features/about/About'
 const Home = () => {
   return (
